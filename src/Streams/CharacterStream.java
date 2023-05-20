e3267d2272169af4269fadda3d9176e6a9010ffc
evgen <parom.evgen@mail.ru>
2023-05-20 18:38:30 +0300

    change charset

--- src/Streams/CharacterStream.java
+++ src/Streams/CharacterStream.java
@@ -5,8 +5,10 @@ import java.io.*;
 public class CharacterStream {
     public static void main(String[] args) {
         try {
-            OutputStreamWriter out = new OutputStreamWriter(new FileOutputStream("example4.txt"));
+            OutputStreamWriter out = new OutputStreamWriter(new FileOutputStream("example4.txt") /*, "ASCII"*/);
             InputStreamReader in = new InputStreamReader(new FileInputStream("example.txt"));
+
+            System.out.println(out.getEncoding());
 
             out.write("hello character3");
 //            out.close();
