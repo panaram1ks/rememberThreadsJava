baae4a308d27eb22ba4dd592703a8e41107d6d29
evgen <parom.evgen@mail.ru>
2023-05-21 08:06:49 +0300

    ServerSocket Socket

---
+++ src/socket/Server.java
@@ -0,0 +1,24 @@
+package socket;
+
+import java.net.*;
+import java.io.*;
+
+
+public class Server {
+
+    public static void main(String[] args) throws Exception{
+        ServerSocket serverSocket = new ServerSocket(9090);
+        System.out.println("waiting for clients...");
+        Socket socket = serverSocket.accept();
+        PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+        out.println("Hello client");
+
+        BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        String clientInput = input.readLine();
+        System.out.println(clientInput);
+
+        input.close();
+        out.close();
+        socket.close();
+    }
+}
