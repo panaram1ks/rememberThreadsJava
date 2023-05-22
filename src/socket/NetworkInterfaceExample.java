02fe05e9b42be5b93a7f9007686fc4a53b17f35b
parom <E.Parominskiy@isida.by>
2023-05-22 09:02:36 +0300

    Networking Interface

---
+++ src/socket/NetworkInterfaceExample.java
@@ -0,0 +1,70 @@
+package socket;
+
+
+import org.apache.commons.validator.routines.InetAddressValidator;
+
+import java.net.InetAddress;
+import java.net.InterfaceAddress;
+import java.net.NetworkInterface;
+import java.util.Enumeration;
+import java.util.List;
+
+public class NetworkInterfaceExample {
+    public static void main(String[] args) {
+        try {
+            InetAddress address = InetAddress.getByName("192.168.1.61");
+
+//1            NetworkInterface networkInterface = NetworkInterface.getByInetAddress(address);
+//2            NetworkInterface networkInterface = NetworkInterface.getByName("enp2s0");
+            Enumeration<NetworkInterface> networkInterfaces = NetworkInterface.getNetworkInterfaces();
+            while (networkInterfaces.hasMoreElements()) {
+                NetworkInterface networkInterface = networkInterfaces.nextElement();
+
+                if (networkInterface != null) {
+                    System.out.println("NIC name: " + networkInterface.getName());
+                    System.out.println("NIC display name: " + networkInterface.getDisplayName());
+                    System.out.println("NIC get hardware address: " + convertByteToString(networkInterface.getHardwareAddress()));
+                    System.out.println("MTU: " + networkInterface.getMTU());
+                    System.out.println("Index: " + networkInterface.getIndex());
+                    NetworkInterface parentInterface = networkInterface.getParent();
+                    if (parentInterface != null) {
+                        System.out.println("Parent interface " + parentInterface.getDisplayName());
+                    } else {
+                        System.out.println("No parent interface");
+                    }
+                    System.out.println("Is loopback? " + networkInterface.isLoopback());
+                    System.out.println("Is up? " + networkInterface.isUp());
+                    System.out.println("Is virtual? " + networkInterface.isVirtual());
+                    System.out.println("Support multicast? " + networkInterface.supportsMulticast());
+
+                    List<InterfaceAddress> list = networkInterface.getInterfaceAddresses();
+                    for (int i = 0; i < list.size(); i++) {
+                        System.out.println("Ip address: " + list.get(i).getAddress().getHostAddress());
+                    }
+                    System.out.println("");
+                    System.out.println("___________________________________________");
+                    System.out.println("");
+                } else {
+                    System.out.println("Interface not found");
+                }
+            }
+        } catch (Exception e) {
+            System.out.println(e.toString());
+        }
+    }
+
+    public static String convertByteToString(byte[] mac) {
+        if (mac == null) {
+            return null;
+        }
+        StringBuilder sb = new StringBuilder(18);
+        for (byte b : mac) {
+            if (sb.length() > 0) {
+                sb.append(':');
+            }
+            sb.append(String.format("%02x", b));
+        }
+        return sb.toString();
+    }
+
+}
